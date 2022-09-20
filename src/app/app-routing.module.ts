1ddcf4bf9124cf3321c655d7d0e4b0f8a173b95f
[baruc] <[barucgs@gmail.com]>
2022-09-20 18:46:20 -0500

    primmer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TemplateModule } from './template/template.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'template' , 
+    loadChildren: ()=> import('./template/template.module').then( m => m.TemplateModule )
+  },
+  {
+    path: 'reactive',
+    loadChildren: ()=> import('./reactive/reactive.module').then( m => m.ReactiveModule )
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
