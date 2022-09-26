41892e8b80f933bd8a717c428d61b1c74b9ff44c
[baruc] <[barucgs@gmail.com]>
2022-09-26 18:20:00 -0500

    final seccion 17 formularios reactivos

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicosComponent } from './basicos/basicos.component';
 import { DinamicosComponent } from './dinamicos/dinamicos.component';
 import { SwitchesComponent } from './switches/switches.component';
+
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { SwitchesComponent } from './switches/switches.component';
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveFormsModule,
+    ReactiveRoutingModule,
+
   ]
 })
 export class ReactiveModule { }
--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true,
   }
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: [ 'M', Validators.required ],
+    notificaciones: [ false, Validators.required ],
+    condiciones: [false, Validators.requiredTrue]
+  });
+
+  constructor( private fb: FormBuilder ) { }
+
+
+  ngOnInit() {
+  
+    this.miFormulario.reset( {
+        ...this.persona,
+        condiciones: false 
+      } );
+
+    this.miFormulario.valueChanges.subscribe( ({ condiciones, ...rest }) =>{
+        // delete condiciones;
+        this.persona = rest;
+        
+    });
+
+  }
+
+  guardar(){
+
+    const formValue = { ...this.miFormulario.value};
+    delete formValue.condiciones;
+
+    this.persona = formValue;
+
+  }
+ 
 
 }
